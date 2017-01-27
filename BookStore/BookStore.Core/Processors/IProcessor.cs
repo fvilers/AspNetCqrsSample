a4eb710aafa93cb7cbc8315433f11e9551d33e34
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 13:16:42 +0100

    Add the processor interface

---
+++ BookStore/BookStore.Core/Processors/IProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Processors
+{
+    public interface IProcessor
+    {
+        Task StartAsync(CancellationToken cancellationToken);
+    }
+}
