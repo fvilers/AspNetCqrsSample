9361cf8f8c2485f87dee71b1968ea8506a7fc8d0
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 11:46:01 +0100

    Add the book domain implementation

---
+++ BookStore/BookStore.Domain/Book.cs
@@ -0,0 +1,67 @@
+﻿using BookStore.Core;
+using BookStore.Core.EventSourcing;
+using BookStore.Domain.Events;
+using System;
+using System.Collections.Generic;
+
+namespace BookStore.Domain
+{
+    public class Book : AggregateRoot
+    {
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Book(string title, string author)
+            : base(Guid.NewGuid())
+        {
+            if (title == null) throw new ArgumentNullException(nameof(title));
+            if (author == null) throw new ArgumentNullException(nameof(author));
+
+            Raise(new BookCreated
+            {
+                Title = title,
+                Author = author
+            });
+        }
+
+        public Book(Guid id, IEnumerable<IVersionedEvent> history)
+            : this(id)
+        {
+            Rehydrate(history);
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price < 0m) throw new ArgumentOutOfRangeException(nameof(price));
+
+            if (price != Price)
+            {
+                Raise(new BookPriceSet
+                {
+                    Price = price
+                });
+            }
+        }
+
+        protected Book(Guid id)
+            : base(id)
+        {
+            Handle<BookCreated>(OnBookCreated);
+            Handle<BookPriceSet>(OnBookPriceSet);
+        }
+
+        private void OnBookPriceSet(BookPriceSet @event)
+        {
+            if (@event == null) throw new ArgumentNullException(nameof(@event));
+            Price = @event.Price;
+        }
+
+        private void OnBookCreated(BookCreated @event)
+        {
+            if (@event == null) throw new ArgumentNullException(nameof(@event));
+            Title = @event.Title;
+            Author = @event.Author;
+        }
+    }
+}
