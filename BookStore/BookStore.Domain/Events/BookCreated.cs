5755ad5c02e8a2aee159b1c5bd9c1e74e97dad07
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 11:41:01 +0100

    Add the book created event

---
+++ BookStore/BookStore.Domain/Events/BookCreated.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Core.EventSourcing;
+
+namespace BookStore.Domain.Events
+{
+    public class BookCreated : VersionedEvent
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
