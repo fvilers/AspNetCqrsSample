6ee43d0763b11d9c0e469302665b103c83ddd51e
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 14:47:35 +0100

    Add the book read model

---
+++ BookStore/BookStore.Api.ReadModels/BookReadModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookStore.Api.ReadModels
+{
+    public class BookReadModel
+    {
+        public Guid AggregateId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
