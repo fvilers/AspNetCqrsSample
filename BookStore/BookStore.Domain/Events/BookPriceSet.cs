6bdec6f28d16a5c88ffb9573a2028458c734fcf0
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 11:41:53 +0100

    Add the book price set event

---
+++ BookStore/BookStore.Domain/Events/BookPriceSet.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Core.EventSourcing;
+
+namespace BookStore.Domain.Events
+{
+    public class BookPriceSet : VersionedEvent
+    {
+        public decimal Price { get; set; }
+    }
+}
