7635286784e3b7b2953103b7c0bf2419852dfee5
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 13:16:49 +0100

    Add a base message processor

---
+++ BookStore/BookStore.Infrastructure/Processors/MessageProcessor.cs
@@ -0,0 +1,53 @@
+﻿using BookStore.Core.Messaging;
+using BookStore.Core.Processors;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Processors
+{
+    public abstract class MessageProcessor<T> : IProcessor
+        where T : IMessage
+    {
+        private readonly IProducerConsumerCollection<Envelope<T>> _commandCollection;
+
+        protected MessageProcessor(IProducerConsumerCollection<Envelope<T>> commandCollection)
+        {
+            if (commandCollection == null) throw new ArgumentNullException(nameof(commandCollection));
+            _commandCollection = commandCollection;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(async () => await StartPollingAsync(cancellationToken), cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Current);
+        }
+
+        protected abstract Task ProcessMessageAsync(T message, string correlationId);
+
+        private async Task StartPollingAsync(CancellationToken cancellationToken)
+        {
+            // TODO: make the poll delay configurable
+            var pollDelay = TimeSpan.FromMilliseconds(250);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Envelope<T> envelope;
+                if (_commandCollection.TryTake(out envelope))
+                {
+                    try
+                    {
+                        await ProcessMessageAsync(envelope.Body, envelope.CorrelationId);
+                    }
+                    catch (Exception)
+                    {
+                        Debugger.Break();
+                    }
+                }
+
+                await Task.Delay(pollDelay, cancellationToken);
+            }
+        }
+    }
+}
