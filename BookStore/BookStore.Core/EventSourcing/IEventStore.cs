7e30046b2248c2f648afff4ae096acf6cfc67083
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 14:17:33 +0100

    Add the event store interface

---
+++ BookStore/BookStore.Core/EventSourcing/IEventStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.EventSourcing
+{
+    public interface IEventStore
+    {
+        Task<IEnumerable<StoredEvent>> FindAsync(Guid aggregateId);
+        Task SaveAsync(IEnumerable<StoredEvent> events);
+    }
+}
---
+++ BookStore/BookStore.Core/EventSourcing/StoredEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookStore.Core.EventSourcing
+{
+    public class StoredEvent
+    {
+        public Guid AggregateId { get; set; }
+        public string AggregateType { get; set; }
+        public int Version { get; set; }
+        public string Payload { get; set; }
+        public string CorrelationId { get; set; }
+    }
+}
