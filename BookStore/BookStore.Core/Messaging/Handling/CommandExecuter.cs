9321f40343f260fa3a2434d9881ed26b803783c9
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 13:35:35 +0100

    Add a command executer implementation

---
+++ BookStore/BookStore.Core/Messaging/Handling/CommandExecuter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Messaging.Handling
+{
+    public class CommandExecuter : ICommandExecuter
+    {
+        private readonly ICommandHandlerRegistry _registry;
+
+        public CommandExecuter(ICommandHandlerRegistry registry)
+        {
+            if (registry == null) throw new ArgumentNullException(nameof(registry));
+            _registry = registry;
+        }
+
+        public async Task ExecuteAsync(ICommand command, string correlationId = null)
+        {
+            if (command == null) throw new ArgumentNullException(nameof(command));
+
+            var commandType = command.GetType();
+            ICommandHandler handler;
+
+            if (_registry.TryGetHandler(commandType, out handler))
+            {
+                await ((ICommandHandler<ICommand>)handler).HandleAsync(command).ConfigureAwait(false);
+            }
+        }
+    }
+}
