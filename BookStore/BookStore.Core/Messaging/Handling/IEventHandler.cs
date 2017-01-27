c668b672c2be0455522eab13c048ebc30739b09b
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 14:58:35 +0100

    Add event handler interface

---
+++ BookStore/BookStore.Core/Messaging/Handling/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace BookStore.Core.Messaging.Handling
+{
+    public interface IEventHandler
+    {
+    }
+
+    public interface IEventHandler<in T> : IEventHandler
+        where T : IEvent
+    {
+        Task HandleAsync(T @event);
+    }
+}
