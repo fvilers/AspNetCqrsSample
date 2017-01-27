26c01da60511df519e7fa191394b96f43ead4a00
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 14:14:49 +0100

    Add the event bus interface

---
+++ BookStore/BookStore.Core/Messaging/IEventBus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Messaging
+{
+    public interface IEventBus
+    {
+        Task PublishAsync(Envelope<IEvent> envelope);
+        Task PublishAsync(IEnumerable<Envelope<IEvent>> envelopes);
+    }
+}
