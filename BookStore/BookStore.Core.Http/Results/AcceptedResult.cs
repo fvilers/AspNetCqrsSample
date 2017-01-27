6388af3a705ac144c63c20179583f38a799fa50c
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 11:05:15 +0100

    Add the accepted result

---
+++ BookStore/BookStore.Core.Http/Results/AcceptedResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace BookStore.Core.Http.Results
+{
+    public class AcceptedResult : StatusCodeResult
+    {
+        public AcceptedResult(HttpRequestMessage request)
+            : base(HttpStatusCode.Accepted, request)
+        {
+        }
+
+        public AcceptedResult(ApiController controller)
+            : base(HttpStatusCode.Accepted, controller)
+        {
+        }
+    }
+}
