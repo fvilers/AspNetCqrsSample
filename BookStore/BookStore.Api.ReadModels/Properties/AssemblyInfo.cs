234156b625f437fbc7e8834fd30ef61e0485acb3
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 14:46:35 +0100

    Add the API read models assembly

---
+++ BookStore/BookStore.Api.ReadModels/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BookStore.Api.ReadModels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dev One")]
+[assembly: AssemblyProduct("BookStore.Api.ReadModels")]
+[assembly: AssemblyCopyright("Copyright © Dev One 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("749b9259-6f17-42b2-b5c8-6f256bdcf808")]
