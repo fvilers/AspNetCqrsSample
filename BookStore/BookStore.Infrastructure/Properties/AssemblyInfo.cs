3035f830a8c5e2b876cbef5743ea2eba20ca94cf
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 10:43:45 +0100

    Add the infrastructure assembly

---
+++ BookStore/BookStore.Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BookStore.Infrastructure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dev One")]
+[assembly: AssemblyProduct("BookStore.Infrastructure")]
+[assembly: AssemblyCopyright("Copyright © Dev One 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f8f132d7-f74b-497a-ab60-e890a7f62c2c")]
