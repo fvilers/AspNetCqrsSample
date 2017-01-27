e349a3bdd37bac9d5fba08e08eac78141315c8d2
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 11:06:24 +0100

    Add the extended api controller

---
+++ BookStore/BookStore.Core.Http/ExtendedApiController.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Core.Http.Results;
+using System.Web.Http;
+
+namespace BookStore.Core.Http
+{
+    public class ExtendedApiController : ApiController
+    {
+        protected IHttpActionResult Accepted()
+        {
+            return new AcceptedResult(this);
+        }
+    }
+}
