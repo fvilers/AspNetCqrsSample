2f76c0842f142db2045faa6919a79d137ba4e496
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 14:50:36 +0100

    Add a generic DAO interface for read models

---
+++ BookStore/BookStore.Core/ReadModels/IDao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.ReadModels
+{
+    public interface IDao<TReadModel>
+    {
+        Task<IEnumerable<TReadModel>> FindAsync();
+        Task<TReadModel> GetAsync(Guid aggregateId);
+        TReadModel Add(Guid aggregateId);
+    }
+}
