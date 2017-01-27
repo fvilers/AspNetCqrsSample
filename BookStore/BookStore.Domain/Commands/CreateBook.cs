5a06aacd0eb28a574bf438e39b6aabc86b21d3a3
Fabian Vilers <fabian.vilers@dev-one.com>
2017-01-27 10:50:06 +0100

    Add the create book command

---
+++ BookStore/BookStore.Domain/Commands/CreateBook.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Core.Messaging;
+
+namespace BookStore.Domain.Commands
+{
+    public class CreateBook : Command
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
